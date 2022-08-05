8c711a48e85ef606f4804afb99233c85080edd12
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-08-05 15:51:55 +0545

    revertng previous changes

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * the top. It is responsible for enqueuing our styles (such as Tailwind CSS),
  * script files (including Alpine), and enabling Blade templating support.
  */
-require_once __DIR__ . '/sawteeTheme/functions.php';
+require_once __DIR__ . '/mountainbreeze/functions.php';
 
 /**
  * Build something amazing...
